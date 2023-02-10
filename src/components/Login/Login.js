b1ccc47b48c8ff1b6f45a590c6c20b0304c6b2fe
Ailing <irene024081@gmail.com>
2023-02-10 10:33:54 -0500

    use setTimeout to implement rebounce and performed cleanup with useEffect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = props => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log('running setTimeout');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('cleanup completed');
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
   const emailChangeHandler = event => {
     setEnteredEmail(event.target.value);
